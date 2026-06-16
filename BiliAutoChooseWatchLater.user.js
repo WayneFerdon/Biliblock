587b5e80b9fa83385ddebe908759af47cd913f2d
WayneFerdon <wayneferdon@hotmail.com>
2026-06-16 21:43:23 +0800

    Update BiliAutoChooseWatchLater.user.js

--- BiliAutoChooseWatchLater.user.js
+++ BiliAutoChooseWatchLater.user.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @name         BiliAutoChooseWatchLater
-// @version      2026.06.03
+// @version      2026.06.16
 // @author       WayneFerdon
 // @include        *.bilibili.com*
 // @exclude      *live.bilibili.com*
 // @exclude      *message.bilibili.com/pages/nav/header_sync*
 // @exclude      *www.bilibili.com/correspond*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @downloadURL https://github.com/WayneFerdon/Biliblock/raw/refs/heads/master/BiliAutoChooseWatchLater.user.js
 // @updateURL https://github.com/WayneFerdon/Biliblock/raw/refs/heads/master/BiliAutoChooseWatchLater.user.js
 // ==/UserScript==
